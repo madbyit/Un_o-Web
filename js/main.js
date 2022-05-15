9fe4d5bbe4ac7248bb58ea37161ac7422a290d66
madbyit <madbyit@bystedts.com>
2022-05-15 15:29:14 +0200

    Add cookie-banner

--- js/main.js
+++ js/main.js
@@ -2,3 +2,15 @@ function getcurrentyear()
 {
    document.getElementById("year").innerHTML = new Date().getFullYear();
 }
+
+function getcookies()
+{
+   var dateCookie = new Date();
+   document.cookie = "Date="+dateCookie;
+   /* By default, the cookie is deleted when the browser is closed. */
+   console.log(document.cookie);
+
+   /* Remove banner */
+   var cb = document.getElementById("cookie-banner");
+   cb.style.display = "none";
+}
