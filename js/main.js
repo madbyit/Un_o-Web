3bc440349252cac39087dec8cd9c5fe9b1150594
Madilyn Bystedt <madilyn.bystedt@unjo.com>
2022-05-16 13:16:41 +0200

    Fix issue with cookie banner timer

--- js/main.js
+++ js/main.js
@@ -7,29 +7,32 @@ function getcurrentyear()
 
 /** Cookies */
 
-function cookiedisplay()
+function setCookies()
 {
-  var now = new Date();
-  var time = now.getTime();
-  var expireTime = time + 1000*36000;
-  now.setTime(expireTime);
-  document.cookie = 'cookie=ok;expires='+now.toUTCString()+';path=/';
-
-  console.log(document.cookie);
-
-  closecookiebanner();
+    var now = new Date();
+    var time = now.getTime();
+    var expireTime = time + 1000*36000;
+    now.setTime(expireTime);
+    document.cookie = 'cookie=ok;expires='+now.toUTCString()+';path=/';
+    console.log(document.cookie);
+    cookieBanner();
 }
 
-function closecookiebanner()
+function cookieBanner()
 {
-   /* Remove banner */
-   var cb = document.getElementById("cookie-banner");
-   cb.style.display = "none";
+  /** Remove banner if there are cookies */
+  if(document.cookie) {
+    console.log("Cookie-value: " + document.cookie);
+    
+    /* Remove banner */
+    var cb = document.getElementById("cookie-banner");
+    cb.style.display = "none";
+  }
 }
 
 /** Slide show */
 
-//let slideIndex = 1;
+let slideIndex = 1;
 
 showSlides(slideIndex);
 
