6dc59e290dac01fa09debe63f0c481562308c3e1
Madilyn Bystedt <madilyn.bystedt@unjo.com>
2022-06-20 15:25:58 +0200

    Fix error

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ function cookieBanner()
 function loadHome() {
   var url = window.location.href; /* Returns full URL */
   var shorturl = url.substring(url.lastIndexOf('/') + 1);
-  if(shorturl == 'index.html' )
+  if((shorturl == 'index.html') || (shorturl == 'Un_o-Web/'))
   {
     var home = document.getElementById("homelink");
     home.click();
